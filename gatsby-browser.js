b5e37b16236bdabc6e2813ef1fb0a06c9cb941a1
emadicio <dicio37@gmail.com>
2019-04-11 15:56:55 +0200

    Client-side locales update support

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,22 @@
-import "./src/styles/global.css";
+const axios = require('axios');
+const flatten = require("flat");
+
+require('./src/styles/global.css');
+
+exports.onClientEntry = async () => {
+    if (process.env.GATSBY_ENV !== 'production') {
+        console.log('[CLIENT] Updating locales...');
+
+        const res = await axios.get('http://18.184.95.18/i18n/ef-stories-2019');
+        const locales = res.data;
+        const languages = Object.keys(locales);
+
+        window.___updatedLocales = {};
+        for (const lang of languages){
+            window.___updatedLocales[lang] = flatten(locales[lang]);
+            window.___updatedLocales[lang]['hero.title'] = 'PROVA';
+        }
+        
+        console.log('[CLIENT] Locales updated.');
+    }
+}
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,21 +1,27 @@
+const dotenv = require('dotenv');
 const fs = require('fs');
 const flatten = require('flat')
 const axios = require('axios');
+const env = process.env.GATSBY_ENV || process.env.NODE_ENV || 'development';
+
+dotenv.config({
+    path: `.env.${env}`,
+}) 
 
 /*
 * Pulls updated locales
 */
 exports.onPreBootstrap = async () => {
-    console.log('Updating locales...');
+    console.log('[SERVER] Updating locales...');
 
     const res = await axios.get('http://18.184.95.18/i18n/ef-stories-2019');
     const locales = res.data;
     const languages = Object.keys(locales);
     
     fs.writeFileSync(`./src/intl/languages.json`, JSON.stringify(languages));
-    for (let lang of languages){
+    for (const lang of languages){
         fs.writeFileSync(`./src/intl/${lang}.json`, JSON.stringify(flatten(locales[lang])));
     }
 
-    console.log('Locales updated.');
+    console.log('[SERVER] Locales updated.');
 }
